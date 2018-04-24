79dbcc3f7bcf5b0127af40ae75db58e1712d37dd
Raymond <stunna617@yahoo.com>
2018-04-24 16:50:48 -0400

    App is working need to style.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
+    zoom: 14,
     center: {
       lat: 42.3601,
       lng: -71.0589
     }
-  })
-  .then(function(response){
-    console.log(response);
-  });
-  .catch(function(error){
-    console.log(error);
   });
   var geocoder = new google.maps.Geocoder();
 
@@ -30,6 +24,11 @@ function geocodeAddress(geocoder, resultsMap) {
         map: resultsMap,
         position: results[0].geometry.location
       });
+      alert(results[0].geometry.location.lat());
+      var lat = results[0].geometry.location.lat()
+      var lng = results[0].geometry.location.lng()
+      document.getElementById('lat').innerHTML = lat
+      document.getElementById('lng').innerHTML = lng
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
